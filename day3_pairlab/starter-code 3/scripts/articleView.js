e216f68fa5e1de515731a95ac1d58d4ba39a0227
Nathan P. Keene <npk@txtincorporated.com>
2016-06-08 14:58:07 -0700

    Nathan drives

--- day3_pairlab/starter-code 3/scripts/articleView.js
+++ day3_pairlab/starter-code 3/scripts/articleView.js
@@ -30,20 +30,27 @@ articleView.populateFilters = function() {
 articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
     if ($(this).val()) {
-      // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
+      var $filterVal = $(this).val();
+            // DONE: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
+      console.log($(this).val());
       $('article').each(function(element) {
         $(this).hide();
       });
       $('article').each(function(element){
-        $(this).attr('');
+        if ($(this).attr('data-author') == $filterVal) {
+          $(this).show();
+        }
       });
     } else {
       // TODO: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
-
-    }
+      $('article').each(function(element){
+        $(this).show();
+      });
+    };
+    
     $('#category-filter').val('');
   });
 };
